18e06348234e9493df5ddd09d497988b9622af5f
oddtomas <oddthomas@csu.fullerton.edu>
2021-08-26 11:53:34 -0700

    finished endpoints for index, create, update, and delete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,30 @@
 class JobsController < ApplicationController
+
+    def index
+        jobs = Job.all
+        render json: jobs
+    end
+  
+    def create
+        job = Job.create(job_params)
+        render json: job
+    end
+  
+    def update
+        job = Job.find(params[:id])
+        job.update(job_params)
+        render json: job
+    end
+  
+    def destroy
+        job = Job.find(params[:id])
+        job.destroy
+        render json: job
+    end
+
+
+    private
+    def job_params
+      params.require(:job).permit( :job_title, :job_description,:job_salary,:might_enjoy, :img)
+    end
 end
