8fe7c79ccd2d28d81ad1b02328f82b3b1bf6724f
oddtomas <oddthomas@csu.fullerton.edu>
2021-08-27 17:00:52 -0700

    added model tests

--- spec/models/dev_spec.rb
+++ spec/models/dev_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Dev, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validatedev_title" do
+   dev = Dev.create(
+    age: 24,
+    technologies: "React, Rails, Postgresql",
+    skills: "teamwork, networking, designing, managing databases",
+    enjoys: 'frontend and backend, and exploring with my dog',
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6JXKaE1oy1Y7to20R9fpWzn11LEB-PZYqDg&usqp=CAU"
+    )
+    expect(dev.errors[:name]).to_not be_empty
+  end
+  it "should validatedev_description" do
+   dev = Dev.create(name: 'Melissa',
+    technologies: "React, Rails, Postgresql",
+    skills: "teamwork, networking, designing, managing databases",
+    enjoys: 'frontend and backend, and exploring with my dog',
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6JXKaE1oy1Y7to20R9fpWzn11LEB-PZYqDg&usqp=CAU"
+    )
+    expect(dev.errors[:age]).to_not be_empty
+  end
+  it "should validatedev_salary" do
+   dev = Dev.create(name: 'Melissa',
+    age: 24,
+    skills: "teamwork, networking, designing, managing databases",
+    enjoys: 'frontend and backend, and exploring with my dog',
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6JXKaE1oy1Y7to20R9fpWzn11LEB-PZYqDg&usqp=CAU"
+    )
+    expect(dev.errors[:technologies]).to_not be_empty
+  end
+  it "should validate might_enjoy" do
+   dev = Dev.create(name: 'Melissa',
+    age: 24,
+    technologies: "React, Rails, Postgresql",
+    enjoys: 'frontend and backend, and exploring with my dog',
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6JXKaE1oy1Y7to20R9fpWzn11LEB-PZYqDg&usqp=CAU"
+    )
+    expect(dev.errors[:skills]).to_not be_empty
+  end
+  it "should validate img" do
+   dev = Dev.create(name: 'Melissa',
+    age: 24,
+    technologies: "React, Rails, Postgresql",
+    skills: "teamwork, networking, designing, managing databases",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6JXKaE1oy1Y7to20R9fpWzn11LEB-PZYqDg&usqp=CAU"
+    )
+    expect(dev.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validatedev_description length to be >= 10 characters" do
+   dev = Dev.create(name: 'Melissa',
+    age: 24,
+    technologies: "React, Rails, Postgresql",
+    skills: "teamwork, networking, designing, managing databases",
+    enjoys: 'frontend and backend, and exploring with my dog',
+    )
+    expect(dev.errors[:img]).to_not be_empty
+  end
 end
